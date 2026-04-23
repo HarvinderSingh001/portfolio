c1540af790fc88d8abeb4252f386c6aee94bd951
harvinder singh <harvinder.singh@shinedezign.com>
2026-04-23 12:25:34 +0530

    favicon fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -62,7 +62,9 @@
 
         <title inertia>Harvinder Singh | Full-Stack Developer</title>
 
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml">
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any">
+        <link rel="apple-touch-icon" href="/favicon.svg?v=2">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap" rel="stylesheet" />
